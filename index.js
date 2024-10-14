6cf435d00331c9fbbea56f2aaafeeb29ea1a47ea
sunanth-samala <sunanth.samala7@email.com>
2024-10-14 10:56:55 +0530

    14

--- index.js
+++ index.js
@@ -26,49 +26,6 @@ app.use("/",(req,res)=>{
 app.use("/hello",(req,res)=>{
   res.send("hello welcome234")
 })
-const connectedUsers = new Map(); // To store connected users
-const connectedDrivers = new Map();
-
-io.on("connection", (socket) => {
-  console.log("a user connected:", socket.id);
-
-  // Handle driver joining their room
- socket.on("joinDriver", (driverId) => {
-    socket.join(driverId);
-    connectedDrivers.set(driverId, socket.id);
-    console.log(`Driver with ID ${driverId} joined, socket ID: ${socket.id}`);
-  });
-   socket.on("joinUser", (userId) => {
-    socket.join(userId);
-    connectedUsers.set(userId, socket.id);
-    console.log(`User with ID ${userId} joined, socket ID: ${socket.id}`);
-  });
-  // Listen for booking request from user
-  socket.on("bookingRequest", (data) => {
-    console.log("Booking request received:", data);
-
-    // Broadcast booking request to multiple drivers
-    data.driverIds.forEach(driverId => {
-      io.to(driverId).emit("newBooking", data);
-    });
-  });
-  socket.on("acceptRequest", ({ userId, driverDetails }) => {
-   
-      io.to(userId).emit("rideAccepted", driverDetails);
-      console.log(`Sent driver details to user ${userId}`);
-    
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
-//dkjhbnskdij
-
-
-
-
-
 
 httpServer.listen(4003, () => {
   console.log("Connected!");
